93f858e54dc45461c85baa73b1c326be781dd056
izzahrani <izzahrani@gmail.com>
2024-10-28 01:59:12 +0800

    add llm.py

--- pages/2_QnA Resale Chatbot.py
+++ pages/2_QnA Resale Chatbot.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 import streamlit as st
+from langchain_openai.chat_models import ChatOpenAI
 
 st.title("QnA Resale Chatbot")
 
@@ -31,3 +32,21 @@ if prompt := st.chat_input("Your message"):
         )
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+
+# def generate_response(input_text):
+#     model = ChatOpenAI(temperature=0.7, api_key=OPENAI_API_KEY)
+#     st.info(model.invoke(input_text))
+
+
+# with st.form("my_form"):
+#     text = st.text_area(
+#         "Enter text:",
+#         "What are the three key pieces of advice for learning how to code?",
+#     )
+#     submitted = st.form_submit_button("Submit")
+#     if not OPENAI_API_KEY.startswith("sk-"):
+#         st.warning("Please enter your OpenAI API key!", icon="⚠")
+#     if submitted and OPENAI_API_KEY.startswith("sk-"):
+#         generate_response(text)
