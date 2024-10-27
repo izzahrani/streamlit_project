d334c09b78cb86e26e73952a810fcade78a415af
izzahrani <izzahrani@gmail.com>
2024-10-28 01:44:33 +0800

    Updated data

--- pages/5_Methodology.py
+++ pages/5_Methodology.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.subheader("Feature 1: QnA Resale Chatbot")
-st.image('data\RAG flowchart.jpg',caption = "Methodology for QnA Resale Chatbot")
+st.image('.\data\RAG flowchart.jpg',caption = "Methodology for QnA Resale Chatbot")
 
 st.subheader("Feature 2: Resale Analyst")
