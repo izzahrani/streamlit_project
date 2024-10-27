4bc1adf0c9c4ad2f61aac9ec86b84d9393ae29f3
izzahrani <izzahrani@gmail.com>
2024-10-28 03:31:40 +0800

    sqlite issue

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
+import sqlite3
 from helper_functions.utility import check_password
 
 # region <--------- Streamlit Page Configuration --------->
